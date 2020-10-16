6334cda5924232f6abb8b67e00546b7fc25090c8
mirfan11 <irfan68.im@gmail.com>
2020-10-16 17:05:44 +0700

    Pertemuan 2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,11 +2,19 @@
 
 class Home extends BaseController
 {
-	public function index()
-	{
-		return view('welcome_message');
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    //--------------------------------------------------------------------
+
+	public function show() {
+        $data['nama'] = 'Muhammad Irfan';
+        $data['npm'] = '1817051071';
+        echo view('mahasiswa/header', $data);
+        echo view('mahasiswa/index', $data);
+        echo view('mahasiswa/footer', $data);
+
 	}
-
-	//--------------------------------------------------------------------
-
 }
